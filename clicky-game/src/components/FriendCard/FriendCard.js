26b3a977bed9c784348c7b326d733e549e6eb4e6
saranasr83 <saranasr83@gmail.comgit config --global user.email saranasr83@gmail.comgit config --global user.email saranasr83@gmail.com>
2018-06-25 23:43:29 -0700

    working on click event

--- clicky-game/src/components/FriendCard/FriendCard.js
+++ clicky-game/src/components/FriendCard/FriendCard.js
@@ -6,9 +6,9 @@ const FriendCard = props => (
     <div className="img-container">
       <img alt={props.name} src={props.image} />
     </div>
-    <span onClick={() => props.removeFriend(props.id)} className="remove">
-      {/* 𝘅 */}
-    </span>
+    {/* <span onClick={() => props.removeFriend(props.id)} className="remove">
+      𝘅
+    </span> */}
   </div>
 );
 
