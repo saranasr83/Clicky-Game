e11b0caee5d23444ee7109a57b2956d3218d0fe0
saranasr83 <saranasr83@gmail.comgit config --global user.email saranasr83@gmail.comgit config --global user.email saranasr83@gmail.com>
2018-06-28 22:10:12 -0700

    work on shuffle function

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,42 +1,72 @@
 import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
+import DisneyChar from "./components/DisneyCharCard/DisneyChar";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import friends from "./friends.json";
+import NavBar from "./components/NavBar";
+import characters from "./disneyChar.json";
 import "./App.css";
 
+//handle the restart button
+
 class App extends Component {
-  // Setting this.state.friends to the friends json array
+  // Setting this.state.characters to the characters json array
   state = {
-    friends
+    characters,
+    currentScore:0,
+    topScore:0,
+    imagesAlreadyClicked:[]
   };
+  //function to shuffle
+// function shuffle(array) {
+  
+// }
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+//function to end the game
+// function endGame(){
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
+// }
+
+  handleImageClick = id => {
+    // shuffle(array)
+    var array=this.state.imagesAlreadyClicked
+    console.log(array)
+
+    for(var i=0;i<array.length;i++){
+      if(id===array[i]){
+        console.log("wrong answer")
+      } else{
+        
+      }
+    }
+    this.setState({
+      currentScore:this.state.currentScore+1
+    })
+    this.state.imagesAlreadyClicked.push(id)
+  }
+
+  // Map over this.state.characters and render a DisneyChar component for each character object
   render() {
     return (
+      <div>
+        <NavBar
+        currentScore={this.state.currentScore}
+        />
       <Wrapper>
+        
         <Title>Clicky Game!</Title>
         <h1>Click on an image to earn points, but don't click on any more than once!</h1>
         <br/>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+        {this.state.characters.map((characters,key) => (
+          <DisneyChar
+            handleImageClick={this.handleImageClick}
+            id={characters.id}
+            key={key}
+            name={characters.name}
+            image={characters.image}
           />
         ))}
       </Wrapper>
+      </div>
     );
   }
 }
