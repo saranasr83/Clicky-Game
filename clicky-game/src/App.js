81949b5e31160b864831969ebe43d70ca66ba31c
saranasr83 <saranasr83@gmail.comgit config --global user.email saranasr83@gmail.comgit config --global user.email saranasr83@gmail.com>
2018-07-05 21:10:58 -0700

    fixed the current score and top score

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -12,71 +12,98 @@ class App extends Component {
   // Setting this.state.characters to the characters json array
   state = {
     characters: characters,
-    currentScore:0,
-    topScore:0,
-    imagesAlreadyClicked:[]
+    currentScore: 0,
+    topScore: 0,
+    imagesAlreadyClicked: [],
+    status: "Click an image to begin!"
   };
   //function to shuffle
-// function shuffle(array) {
-  
-// }
+  // function shuffle(array) {
 
-//function to end the game
-// function endGame(){
+  // }
 
-// }
+  //function to end the game
+  // function endGame(){
+
+  // }
 
   handleImageClick = id => {
-    // shuffle(array)
+    var that=this
     const charArray = this.state.imagesAlreadyClicked;
     console.log("***", this.state.imagesAlreadyClicked);
-    console.log("####",charArray.includes(id));
+    console.log("####", charArray.includes(id));
 
-    if(charArray.includes(id)){
-      this.endGame();
+    if (charArray.includes(id)) {
+      if (this.state.currentScore>this.state.topScore){
+        this.setState({
+          topScore: this.state.currentScore
+        })
+      }
+      this.setState({
+        status: "You guessed incorrect!",
+        
+      })
+      
+      document.querySelector("#status").classList.add("red-text")
+      setTimeout(function () {
+        document.querySelector("#status").classList.remove("red-text")
+        that.setState({
+          
+          currentScore: 0,
+          imagesAlreadyClicked: [],
+          status:"Click any image to begin!"
+        })
+      }, 500)
+      
     }
-    else{
+    else {
       this.state.imagesAlreadyClicked.push(id)
       this.setState({
-        currentScore:this.state.currentScore+1
+        currentScore: this.state.currentScore + 1,
+        status: "You guessed correctly!"
       });
+      document.querySelector("#status").classList.add("green-text")
+      setTimeout(function () {
+        document.querySelector("#status").classList.remove("green-text")
+      }, 500)
     }
-    
+    this.endGame();
   }
 
 
 
-  endGame(){
+  endGame() {
     // check if currentScore is larger than topScore
-    
-    if(this.state.currentScore > this.state.topScore){
+
+    if (this.state.currentScore > 12) {
       let score = this.state.currentScore
       this.setState({
         topScore: score,
         currentScore: 0,
-        imagesAlreadyClicked: []
+        imagesAlreadyClicked: [],
+        status:"Click any image to begin!"
       })
     }
     // take the current characters array and reorder
-    
+
   }
 
   shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-  
+
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-  
+
       // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-  
+
       // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
     }
-  
+
     return array;
   }
   // Map over this.state.characters and render a DisneyChar component for each character object
@@ -84,24 +111,25 @@ class App extends Component {
     return (
       <div>
         <NavBar
-        currentScore={this.state.currentScore}
-        topScore={this.state.topScore}
+          currentScore={this.state.currentScore}
+          topScore={this.state.topScore}
+          status={this.state.status}
         />
-      <Wrapper>
-        
-        <Title>Clicky Game!</Title>
-        <h1>Click on an image to earn points, but don't click on any more than once!</h1>
-        <br/>
-        {this.state.characters.map((characters,key) => (
-          <DisneyChar
-            handleImageClick={this.handleImageClick}
-            id={characters.id}
-            key={key}
-            name={characters.name}
-            image={characters.image}
-          />
-        ))}
-      </Wrapper>
+        <Wrapper>
+
+          <Title>Clicky Game!</Title>
+          <h1>Click on an image to earn points, but don't click on any more than once!</h1>
+          <br />
+          {this.state.characters.map((characters, key) => (
+            <DisneyChar
+              handleImageClick={this.handleImageClick}
+              id={characters.id}
+              key={key}
+              name={characters.name}
+              image={characters.image}
+            />
+          ))}
+        </Wrapper>
       </div>
     );
   }
--- clicky-game/src/components/NavBar/NavBar.js
+++ clicky-game/src/components/NavBar/NavBar.js
@@ -4,11 +4,12 @@ import "./NavBar.css";
 const NavBar = props => (
     <div className="sticky-nav">
         <div className="nav-title">
-            <h1>Click an image to begin!</h1>
+            <h1 id="status">{props.status}</h1>
             <a className="nav-restart" href="/">Restart</a>
             <div className="scores">
-            <div className="score" >Current Score: {props.currentScore}</div>
+            
             <div className="score" > | Top Score: {props.topScore}</div>
+            <div className="score" >Current Score: {props.currentScore}</div>
             </div>
         </div>
     </div>
